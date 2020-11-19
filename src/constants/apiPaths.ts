38e5c19df254eebe6caeb98b6153c76e96edc3cc
Konstantin Zharich <kostyasupikos@gmail.com>
2020-11-19 12:08:26 +0300

    [AWS-TASK-5] - import-service FE part
    - changed url and path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   products: 'https://uxpaqh5vhg.execute-api.eu-west-1.amazonaws.com/dev/products',
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://uxpaqh5vhg.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://sw1yos7ub8.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://uxpaqh5vhg.execute-api.eu-west-1.amazonaws.com/dev/'
 };
 
